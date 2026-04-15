cdda6519431132a2d5f44302150d4160a647d48c
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-16 00:42:35 +0530

    Time: 236 ms (27.78%) | Memory: 171.4 MB (57.52%) - LeetSync

---
+++ 2012-process-tasks-using-servers/process-tasks-using-servers.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] assignTasks(int[] servers, int[] tasks) {
+        PriorityQueue<int[]> pq= new PriorityQueue<>(
+            (a,b)-> a[0]==b[0] ? a[1]-b[1] : a[0]-b[0]
+        );
+        int extendedServer[][]= new int[servers.length][3];
+        for(int i=0;i<servers.length;i++){
+            extendedServer[i][0]=servers[i];
+            extendedServer[i][1]=i;
+            extendedServer[i][2]=0;
+            pq.offer(extendedServer[i]);
+        }
+
+        PriorityQueue<int[]> remtime=new PriorityQueue<>(
+            (a,b)-> a[2]==b[2] ? a[0]-b[0] : a[2]-b[2]
+        );
+
+        int[] ans= new int[tasks.length];
+        int currTime=0;
+        int taskIdx=0;
+        while(taskIdx<tasks.length){
+            while(!remtime.isEmpty() && remtime.peek()[2]<=currTime){
+                pq.offer(remtime.poll());
+            }
+            while(!pq.isEmpty() && taskIdx<=currTime){
+                int[] arr=pq.poll();
+                arr[2]=currTime+tasks[taskIdx];
+                ans[taskIdx++]=arr[1];
+                if(taskIdx==tasks.length) return ans;
+                remtime.offer(arr);
+            }
+
+            if(pq.isEmpty()) currTime=Math.max(currTime,remtime.peek()[2]);
+            else currTime=Math.max(currTime,taskIdx);
+            
+        }
+        return ans;
+    }
+}
