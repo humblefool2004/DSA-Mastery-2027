4472cf693ea01cf9ef8fb9a11f85226e3eb016ba
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-16 23:43:36 +0530

    Time: 8 ms (91.31%) | Memory: 48.8 MB (84.95%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,37 +1,4 @@
-//dual array approach,to avoid sorting 2D array
-class Solution {
-    public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        if (n <= 1) return intervals;
 
-        int[] starts = new int[n];
-        int[] ends = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-
-        // Sorting primitives is significantly faster than sorting int[][]
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-
-        List<int[]> result = new ArrayList<>();
-        int startIndex = 0;
-
-        for (int i = 0; i < n; i++) {
-            // If the current start is the last one OR 
-            // the next start is greater than the current end, we found a gap.
-            if (i == n - 1 || starts[i + 1] > ends[i]) {
-                result.add(new int[]{starts[startIndex], ends[i]});
-                startIndex = i + 1;
-            }
-        }
-
-        return result.toArray(new int[result.size()][]);
-    }
-}
-/*
 //Single array optimization. (91.31,84.95)
 class Solution {
     public int[][] merge(int[][] intervals) {
@@ -40,25 +7,17 @@ class Solution {
         );
 
         int n=intervals.length;
-        int[] currInterval = new int[]{
-            intervals[0][0],
-            intervals[0][1]
-        };
-        int[][] ans= new int[n][2];
-        int intervalCount=0;
+        int[] currInterval = intervals[0];
+        List<int[]> ans= new ArrayList<>();
         for(int i=1;i<n;i++){
             if(currInterval[1]>=intervals[i][0]){
                 currInterval[1] = Math.max(currInterval[1],intervals[i][1]);
             }else{
-                ans[intervalCount++]=currInterval;
-                currInterval=new int[]{
-                    intervals[i][0],
-                    intervals[i][1]
-                };
+                ans.add(currInterval);
+                currInterval=intervals[i];
             }
         }
-        ans[intervalCount++]=currInterval;
-        return Arrays.copyOfRange(ans,0,intervalCount);
+        ans.add(currInterval);
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
-*/
