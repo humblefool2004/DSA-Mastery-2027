522400047587c8c4ba60965a883b6112cf72c142
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-16 22:19:18 +0530

    Time: 151 ms (5.03%) | Memory: 54.3 MB (56.22%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,3 +1,47 @@
+//quickselect approach
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        quickSelect(points,0,points.length-1,k);
+        return Arrays.copyOfRange(points,0,k);
+    }
+
+    private void quickSelect(int[][] points,int left, int right,int k){
+        if(left>=right) return;
+        int pivotIndex=partition(points,left,right);
+        if(pivotIndex==k){
+            return;
+        }else if(pivotIndex>k){
+            quickSelect(points,left,pivotIndex-1,k);
+        }else{
+            quickSelect(points,pivotIndex+1,right,k);
+        }
+    }
+
+    private int partition(int[][] points, int left,int right){
+        int pivot=getDist(points[right]);
+        int i=left;
+        for(int j=left;j<right;j++){
+            if(getDist(points[j])<=pivot){
+                swap(points,i,j);
+                i++;
+            }
+        }
+        swap(points,i,right);
+        return i;
+    }
+
+    private int getDist(int[] point){
+        return point[0]*point[0] + point[1]*point[1]; 
+    }
+    private void swap(int[][] points,int i,int j){
+        int[] temp= points[i];
+        points[i]= points[j];
+        points[j]=temp;
+    }
+}
+
+/*
+//Priority Queue approach
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> pq= new PriorityQueue<>(
@@ -22,3 +66,4 @@ class Solution {
         return answer;
     }
 }
+*/
