d1b9920013dc84bf62dc9a37eeded6973308c076
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-16 23:57:58 +0530

    Time: 7 ms (98.84%) | Memory: 49.1 MB (44.07%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,4 +1,29 @@
+//dual array approach,to avoid sorting 2D array
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        int n= intervals.length;
+        int start[]=new int[n];
+        int end[]=new int[n];
+        for(int i=0;i<n;i++){
+            start[i]=intervals[i][0];
+            end[i]=intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        List<int[]> result = new ArrayList<>();
+        int startIndex=0;
+        for(int i=0;i<n;i++){
+            if(i==n-1 || start[i+1]>end[i]){
+                result.add(new int[]{start[startIndex],end[i]});
+                startIndex=i+1;
+            }
+        }
+        return result.toArray(new int[result.size()][2]);
 
+    }
+}
+
+/*
 //Single array optimization. (91.31,84.95)
 class Solution {
     public int[][] merge(int[][] intervals) {
@@ -21,3 +46,4 @@ class Solution {
         return ans.toArray(new int[ans.size()][2]);
     }
 }
+*/
