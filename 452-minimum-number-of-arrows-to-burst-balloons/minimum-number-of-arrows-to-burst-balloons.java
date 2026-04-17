32ec0cea4f3066d673f36f62195473f7fc9e22ec
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-17 23:10:51 +0530

    Time: 53 ms (50.25%) | Memory: 95.7 MB (54.41%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,14 +1,5 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        //lets find non overlapping single arrow intervals
-        //1 2 7  10
-        //6 8 12 16
-
-        //1 3 5 7
-        //2 4 6 8
-
-        //1 2 3 4
-        //2 3 4 5
         int n= points.length;
         Arrays.sort(points, 
         (a,b)-> Integer.compare(a[1],b[1])
