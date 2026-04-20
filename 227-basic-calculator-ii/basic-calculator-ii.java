78da63fdc6f63b83b14ddf704390edf5c020d7b4
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-20 12:04:43 +0530

    Time: 24 ms (38.36%) | Memory: 47.9 MB (72.31%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,65 +1,104 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Character> ch = new Stack<>();
         Stack<Integer> st = new Stack<>();
-        s = s.replaceAll(" ", "");
+        int currNum = 0;
+        char op = '+';
 
         for (int i = 0; i < s.length(); i++) {
-            char ss = s.charAt(i);
-            if (ss == '+' || ss == '-' || ss == '*' || ss == '/') {
-                if (ch.isEmpty())
-                    ch.push(ss);
-                else if (ss == '*' || ss == '/') {
-                    while (!ch.isEmpty() && (ch.peek() == '*' || ch.peek() == '/')) {
-                        char op = ch.pop();
-                        int b = st.pop();
-                        int a = st.pop();
-                        int res = switch (op) {
-                            case '*' -> a * b;
-                            case '/' -> a / b;
-                            default -> 0;
-                        };
-                        st.push(res);
-                    }
-                    ch.push(ss);
-                } else {
-                    while (!ch.isEmpty()) {
-                        char op = ch.pop();
-                        int b = st.pop();
-                        int a = st.pop();
-                        int res = switch (op) {
-                            case '*' -> a * b;
-                            case '/' -> a / b;
-                            case '-' -> a - b;
-                            case '+' -> a + b;
-                            default -> 0;
-                        };
-                        st.push(res);
-                    }
-                    ch.push(ss);
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                currNum = currNum * 10 + (ch - '0');
+            }
+
+            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
+
+                if (op == '+') {
+                    st.push(currNum);
+                } else if (op == '-') {
+                    st.push(-currNum);
+                } else if (op == '*') {
+                    st.push(st.pop() * currNum);
+                } else if (op == '/') {
+                    st.push(st.pop() / currNum);
                 }
-            } else {
-                int start = i;
-                while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
-                    i++;
-                }
-                String number = s.substring(start, i + 1);
-                st.push(Integer.parseInt(number));
+
+                op = ch;
+                currNum = 0;
             }
         }
-        while (!ch.isEmpty()) {
-            char op = ch.pop();
-            int b = st.pop();
-            int a = st.pop();
-            int res = switch (op) {
-                case '*' -> a * b;
-                case '/' -> a / b;
-                case '-' -> a - b;
-                case '+' -> a + b;
-                default -> 0;
-            };
-            st.push(res);
+
+        int ans = 0;
+        while (!st.isEmpty()) {
+            ans += st.pop();
         }
-        return st.peek();
+        return ans;
     }
 }
+
+//Overly Engineered approach
+// class Solution {
+//     public int calculate(String s) {
+//         Stack<Character> ch = new Stack<>();
+//         Stack<Integer> st = new Stack<>();
+//         s = s.replaceAll(" ", "");
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ss = s.charAt(i);
+//             if (ss == '+' || ss == '-' || ss == '*' || ss == '/') {
+//                 if (ch.isEmpty())
+//                     ch.push(ss);
+//                 else if (ss == '*' || ss == '/') {
+//                     while (!ch.isEmpty() && (ch.peek() == '*' || ch.peek() == '/')) {
+//                         char op = ch.pop();
+//                         int b = st.pop();
+//                         int a = st.pop();
+//                         int res = switch (op) {
+//                             case '*' -> a * b;
+//                             case '/' -> a / b;
+//                             default -> 0;
+//                         };
+//                         st.push(res);
+//                     }
+//                     ch.push(ss);
+//                 } else {
+//                     while (!ch.isEmpty()) {
+//                         char op = ch.pop();
+//                         int b = st.pop();
+//                         int a = st.pop();
+//                         int res = switch (op) {
+//                             case '*' -> a * b;
+//                             case '/' -> a / b;
+//                             case '-' -> a - b;
+//                             case '+' -> a + b;
+//                             default -> 0;
+//                         };
+//                         st.push(res);
+//                     }
+//                     ch.push(ss);
+//                 }
+//             } else {
+//                 int start = i;
+//                 while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
+//                     i++;
+//                 }
+//                 String number = s.substring(start, i + 1);
+//                 st.push(Integer.parseInt(number));
+//             }
+//         }
+//         while (!ch.isEmpty()) {
+//             char op = ch.pop();
+//             int b = st.pop();
+//             int a = st.pop();
+//             int res = switch (op) {
+//                 case '*' -> a * b;
+//                 case '/' -> a / b;
+//                 case '-' -> a - b;
+//                 case '+' -> a + b;
+//                 default -> 0;
+//             };
+//             st.push(res);
+//         }
+//         return st.peek();
+//     }
+// }
