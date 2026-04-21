1863efdd0fb8dc76f97387b66c2b5457162bc2f5
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-21 21:10:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (54.87%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -2,9 +2,9 @@ class Solution {
     public int trailingZeroes(int n) {
         int ans=0;
         int i=1;
-        while(n>=Math.pow(5,i)){
-            ans+=n/Math.pow(5,i);
-            i++;
+        while(n>0){
+            n=n/5;
+            ans+=n;
         }
         return ans;
     }
