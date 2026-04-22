65155c754a47b502846586d90d1b955ccc0b5d04
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-23 02:17:00 +0530

    Time: 1 ms (99.81%) | Memory: 46.6 MB (88.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,22 +10,59 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        //naive approach, putting everything inside a min heap, then extracting one by one.
-        PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)->
-            Integer.compare(a.val,b.val)
-        );
+        if(lists.length==0) return null;
+        //mergeSort approach;
+        return mergeSort(lists,0,lists.length-1);
 
-        for(ListNode node : lists){
-            if(node!=null) pq.add(node);
-        }
-        ListNode answer=new ListNode(0);
-        ListNode temp= answer;
-        while(!pq.isEmpty()){
-            ListNode node=pq.poll();
-            temp.next=node;
+    }
+    private ListNode mergeSort(ListNode[] lists,int i, int n){
+        if(n-i==0) return lists[i];
+        int mid= i+ (n-i)/2;
+        ListNode left= mergeSort(lists,i,mid);
+        ListNode right= mergeSort(lists,mid+1,n);
+
+        return merge(left,right);
+    }
+
+
+    private ListNode merge(ListNode a,ListNode b){
+        ListNode ans=new ListNode(0);
+        ListNode temp=ans;
+        while(a!=null  && b!=null){
+            if(a.val >b.val){
+                temp.next=b;
+                b=b.next;
+            }
+            else{
+                temp.next=a;
+                a=a.next;
+            }
             temp=temp.next;
-            if(node.next!=null) pq.add(node.next);
         }
-        return answer.next;
+        temp.next= (a!=null)? a : b;
+        return ans.next;
     }
 }
+
+//naive approach, putting everything inside a min heap, then extracting one by one.
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+        
+//         PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)->
+//             Integer.compare(a.val,b.val)
+//         );
+
+//         for(ListNode node : lists){
+//             if(node!=null) pq.add(node);
+//         }
+//         ListNode answer=new ListNode(0);
+//         ListNode temp= answer;
+//         while(!pq.isEmpty()){
+//             ListNode node=pq.poll();
+//             temp.next=node;
+//             temp=temp.next;
+//             if(node.next!=null) pq.add(node.next);
+//         }
+//         return answer.next;
+//     }
+// }
