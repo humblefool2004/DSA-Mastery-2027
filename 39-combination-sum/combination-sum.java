3e5a079a09c0622b9cd72d521b2868a73652a27f
Saheel Mohanta <humblefool2004@gmail.com>
2026-04-25 20:22:34 +0530

    Time: 3 ms (34.15%) | Memory: 46 MB (20.11%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,24 +1,52 @@
+//pruning approach
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans= new ArrayList<>();
+        Arrays.sort(candidates);
         getCombinations(ans,candidates,0,target,new ArrayList<>());
         return ans;
     }
 
     private void getCombinations(List<List<Integer>> ans,int[] candidates, int i, int target, List<Integer> arr){
-        if(i==candidates.length) {
-            if(target==0) ans.add(new ArrayList<>(arr));
+        if(target==0){
+            ans.add(new ArrayList<>(arr));
+            return;
+        } 
+        if(i==candidates.length || candidates[i]>target) {
             return;
         }
         //pick
-        if(candidates[i]<=target){
             arr.add(candidates[i]);
             getCombinations(ans,candidates,i,target-candidates[i],arr);
             arr.removeLast();
-        }
 
         //not pick;
         getCombinations(ans,candidates,i+1,target,arr);
 
     }
 }
+// search all
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         List<List<Integer>> ans= new ArrayList<>();
+//         getCombinations(ans,candidates,0,target,new ArrayList<>());
+//         return ans;
+//     }
+
+//     private void getCombinations(List<List<Integer>> ans,int[] candidates, int i, int target, List<Integer> arr){
+//         if(i==candidates.length) {
+//             if(target==0) ans.add(new ArrayList<>(arr));
+//             return;
+//         }
+//         //pick
+//         if(candidates[i]<=target){
+//             arr.add(candidates[i]);
+//             getCombinations(ans,candidates,i,target-candidates[i],arr);
+//             arr.removeLast();
+//         }
+
+//         //not pick;
+//         getCombinations(ans,candidates,i+1,target,arr);
+
+//     }
+// }
