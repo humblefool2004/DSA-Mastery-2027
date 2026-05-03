2e635c8de9eb408197ac01aa0719da77688a6471
Saheel Mohanta <humblefool2004@gmail.com>
2026-05-03 23:38:31 +0530

    Time: 2 ms (70.90%) | Memory: 55.8 MB (31.95%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority=nums[0];
+        int track=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=majority){
+                track--;
+            }else track++;
+            if(track==-1){
+                majority=nums[i];
+                track=1;
+            }
+        }
+        return majority;
+    }
+}
