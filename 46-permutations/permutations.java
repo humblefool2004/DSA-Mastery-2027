98d8ff747972ce20001575e0c9cb0373585472c9
Saheel Mohanta <humblefool2004@gmail.com>
2026-05-04 17:17:57 +0530

    Time: 1 ms (86.24%) | Memory: 45.7 MB (30.00%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,52 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
-        permutations(ans,nums,new ArrayList<>(),new boolean[nums.length]);
+        permutations(ans,nums,0,new ArrayList<>());
         return ans;
     }
 
-    private void permutations(List<List<Integer>> ans, int[] nums, List<Integer> list,boolean freq[]){
-        if(list.size()==nums.length){
+    private void swap(int nums[],int a, int b){
+        int temp= nums[a];
+        nums[a]= nums[b];
+        nums[b]= temp;
+    }
+
+    private void permutations(List<List<Integer>> ans, int nums[],int start, List<Integer> list){
+        if(start==nums.length){
             ans.add(new ArrayList<>(list));
             return;
-        } 
-        for(int i=0;i<nums.length;i++){
-            if(freq[i]==true) continue;
-
-            list.add(nums[i]);
-            freq[i]=true;
-            permutations(ans,nums,list,freq);
+        }
+        for(int i=start;i<nums.length;i++){
+            swap(nums,start,i);
+            list.add(nums[start]);
+            permutations(ans,nums,start+1,list);
             list.removeLast();
-            freq[i]=false;
+            swap(nums,start,i);
         }
     }
 }
+
+//takes extra space
+// class Solution {
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> ans= new ArrayList<>();
+//         permutations(ans,nums,new ArrayList<>(),new boolean[nums.length]);
+//         return ans;
+//     }
+
+//     private void permutations(List<List<Integer>> ans, int[] nums, List<Integer> list,boolean freq[]){
+//         if(list.size()==nums.length){
+//             ans.add(new ArrayList<>(list));
+//             return;
+//         } 
+//         for(int i=0;i<nums.length;i++){
+//             if(freq[i]==true) continue;
+
+//             list.add(nums[i]);
+//             freq[i]=true;
+//             permutations(ans,nums,list,freq);
+//             list.removeLast();
+//             freq[i]=false;
+//         }
+//     }
+// }
