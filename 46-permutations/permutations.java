93a4e628e854ce32b9b2913cb84b5d7cd6b08c15
Saheel Mohanta <humblefool2004@gmail.com>
2026-05-04 17:00:42 +0530

    Time: 1 ms (86.24%) | Memory: 45.4 MB (62.96%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans= new ArrayList<>();
-        permutations(ans,nums,new ArrayList<>(),new HashSet<>());
+        permutations(ans,nums,new ArrayList<>(),new boolean[nums.length]);
         return ans;
     }
 
-    private void permutations(List<List<Integer>> ans, int[] nums, List<Integer> list,Set<Integer> set){
-        if(set.size()==nums.length){
+    private void permutations(List<List<Integer>> ans, int[] nums, List<Integer> list,boolean freq[]){
+        if(list.size()==nums.length){
             ans.add(new ArrayList<>(list));
             return;
         } 
         for(int i=0;i<nums.length;i++){
-            if(set.contains(i)) continue;
+            if(freq[i]==true) continue;
+
             list.add(nums[i]);
-            set.add(i);
-            permutations(ans,nums,list,set);
+            freq[i]=true;
+            permutations(ans,nums,list,freq);
             list.removeLast();
-            set.remove(i);
+            freq[i]=false;
         }
     }
 }
