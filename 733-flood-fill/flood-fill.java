12f252deea38a10f349959537162e0ac693ddbe5
Saheel Mohanta <humblefool2004@gmail.com>
2026-05-12 23:23:26 +0530

    Time: 1 ms (36.52%) | Memory: 47 MB (24.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -5,10 +5,8 @@ class Solution {
 
         int m= image.length;
         int n= image[0].length;
-        boolean[] visited= new boolean[m*n];
 
         Queue<Integer> queue= new ArrayDeque<>(); 
-        visited[sr*n+sc]=true;
         queue.add(sr*n + sc);
 
         while(!queue.isEmpty()){
@@ -16,28 +14,24 @@ class Solution {
             int r=curr/n;
             int c= curr%n;
             image[r][c]=color;
-            if(validEdge(image,m,n,r,c+1,visited,initialColor)){
+            if(validEdge(image,m,n,r,c+1,initialColor)){
                 queue.add(r*n +c+1);
-                visited[r*n+c+1]=true;
             }
-            if(validEdge(image,m,n,r,c-1,visited,initialColor)){
+            if(validEdge(image,m,n,r,c-1,initialColor)){
                 queue.add(r*n +c-1);
-                visited[r*n +c-1]=true;
             }
-            if(validEdge(image,m,n,r+1,c,visited,initialColor)){
+            if(validEdge(image,m,n,r+1,c,initialColor)){
                 queue.add((r+1)*n +c);
-                visited[(r+1)*n +c]=true;
             }
-            if(validEdge(image,m,n,r-1,c,visited,initialColor)){
+            if(validEdge(image,m,n,r-1,c,initialColor)){
                 queue.add((r-1)*n +c);
-                visited[(r-1)*n +c]=true;
             }  
         }
         return image;
     }
 
-    private boolean validEdge(int image[][], int m, int n, int i,int j, boolean[] visited,int initialColor){
+    private boolean validEdge(int image[][], int m, int n, int i,int j,int initialColor){
         if(i>= m || i<0 || j>=n || j<0) return false;
-        return !visited[i*n + j] && image[i][j]==initialColor;
+        return image[i][j]==initialColor;
     }
 }
