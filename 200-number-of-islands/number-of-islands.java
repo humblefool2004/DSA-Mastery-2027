117d08f2053f1e6528b06c28a3a99b09c1157508
Saheel Mohanta <humblefool2004@gmail.com>
2026-05-27 20:56:56 +0530

    Time: 2 ms (99.79%) | Memory: 52.4 MB (33.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,4 @@
 class Solution {
-    private int dir[][]={
-        {1,0},
-        {0,1},
-        {-1,0},
-        {0,-1}
-    };
-
     public int numIslands(char[][] grid) {
         int m=grid.length;
         int n= grid[0].length;
@@ -14,21 +7,18 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j);
+                    dfs(grid,i,j,m,n);
                 }
             }
         }
         return count;
     }
 
-    private void dfs(char[][] grid, int i,int j){
-        for(int[] d : dir){
-            int ni=d[0] + i;
-            int nj= d[1]+ j;
-            if(ni>=0 && ni<grid.length && nj>=0 && nj<grid[0].length && grid[ni][nj]=='1'){
-                grid[ni][nj]='2';
-                dfs(grid,ni,nj);
-            }
-        }
+    private void dfs(char[][] grid, int i,int j,int m,int n){
+        grid[i][j]='2';
+        if(i+1 <m && grid[i+1][j]=='1') dfs(grid,i+1,j,m,n);
+        if(j+1 <n && grid[i][j+1]=='1') dfs(grid,i,j+1,m,n);
+        if(i-1 >=0 && grid[i-1][j]=='1') dfs(grid,i-1,j,m,n);
+        if(j-1 >=0 && grid[i][j-1]=='1') dfs(grid,i,j-1,m,n);
     }
 }
