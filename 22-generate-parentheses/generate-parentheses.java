88c4c46e598800da8361815fef6d2b271711457f
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-14 19:58:30 +0530

    Time: 3 ms (15.28%) | Memory: 44.5 MB (73.81%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> answer= new ArrayList<>();
+        generate(0,0,n,new StringBuilder(),answer);
+        System.out.println(answer);
+        return answer;
+    }
+
+    private void generate(int open, int close, int n,StringBuilder sb,List<String> ans){
+        if(open==n && close ==n){
+            ans.add(sb.toString());
+            return;
+        }
+
+        if(open<n){
+            sb.append('(');
+            generate(open+1,close, n,sb,ans);
+            sb.setLength(sb.length()-1);
+        }
+        if(close<open){
+            sb.append(')');
+            generate(open,close+1, n,sb,ans);
+            sb.setLength(sb.length()-1);
+        }
+    }
+}
