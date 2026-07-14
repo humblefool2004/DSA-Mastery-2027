e3ed1672f6a53cea92d9084f68cd551d7efbc27a
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-14 20:06:04 +0530

    Time: 1 ms (86.80%) | Memory: 44.3 MB (80.82%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -2,7 +2,6 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> answer= new ArrayList<>();
         generate(0,0,n,new StringBuilder(),answer);
-        System.out.println(answer);
         return answer;
     }
 
@@ -15,12 +14,12 @@ class Solution {
         if(open<n){
             sb.append('(');
             generate(open+1,close, n,sb,ans);
-            sb.setLength(sb.length()-1);
+            sb.deleteCharAt(sb.length()-1);
         }
         if(close<open){
             sb.append(')');
             generate(open,close+1, n,sb,ans);
-            sb.setLength(sb.length()-1);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
