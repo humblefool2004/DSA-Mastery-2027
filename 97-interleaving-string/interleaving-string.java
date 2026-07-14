f64cf905b20ce4ceb1af9fce04790327888850d8
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-14 19:11:08 +0530

    Time: 1 ms (92.90%) | Memory: 43.5 MB (28.74%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -9,15 +9,16 @@ class Solution {
     private boolean helper(int i, int j, String s1,String s2,String s3,Boolean memo[][]){
         
         if(i==s1.length() && j==s2.length()) return true;
-
+        if (memo[i][j] != null)
+            return memo[i][j];
         if(i<s1.length()
-            && s1.charAt(i)==s3.charAt(i+j) && memo[i][j]==null && helper(i+1,j,s1,s2,s3,memo)
+            && s1.charAt(i)==s3.charAt(i+j) && helper(i+1,j,s1,s2,s3,memo)
             ){
                 memo[i][j]=true;
             return true;
         }
         if(j<s2.length()
-            && s2.charAt(j)==s3.charAt(i+j) && memo[i][j]==null && helper(i,j+1,s1,s2,s3,memo)
+            && s2.charAt(j)==s3.charAt(i+j) && helper(i,j+1,s1,s2,s3,memo)
             ) {
                 memo[i][j]=true;
             return true;
