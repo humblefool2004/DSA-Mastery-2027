843413a604de3e94f7a9e3808baaaaf04c201c98
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-14 19:08:41 +0530

    Time: 1 ms (92.90%) | Memory: 43.5 MB (28.74%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length() + s2.length() != s3.length())
+            return false;
+        Boolean memo[][] = new Boolean[s1.length()+1][s2.length()+1];
+        return helper(0,0,s1,s2,s3,memo);
+    }
+
+    private boolean helper(int i, int j, String s1,String s2,String s3,Boolean memo[][]){
+        
+        if(i==s1.length() && j==s2.length()) return true;
+
+        if(i<s1.length()
+            && s1.charAt(i)==s3.charAt(i+j) && memo[i][j]==null && helper(i+1,j,s1,s2,s3,memo)
+            ){
+                memo[i][j]=true;
+            return true;
+        }
+        if(j<s2.length()
+            && s2.charAt(j)==s3.charAt(i+j) && memo[i][j]==null && helper(i,j+1,s1,s2,s3,memo)
+            ) {
+                memo[i][j]=true;
+            return true;
+        }
+        memo[i][j]=false;
+        return false;
+    }
+}
