669349bb414d8c7f4cada390c221d7bfb52a449c
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-23 13:53:18 +0530

    Time: 100 ms (49.32%) | Memory: 87.2 MB (28.22%) - LeetSync

--- 3151-minimum-processing-time/minimum-processing-time.java
+++ 3151-minimum-processing-time/minimum-processing-time.java
@@ -8,12 +8,9 @@ class Solution {
         int ans=Integer.MIN_VALUE;
 
         for(int num : processorTime){
-            int max=Integer.MIN_VALUE;
-            for(int i=0;i<4;i++){
-                int a=tasks.get(completed++);
-                max= Math.max(a,max);
-            }
-            ans=Math.max(ans,max+num);                
+            
+            ans=Math.max(ans,tasks.get(completed)+num);                
+            completed+=4;
         }
         return ans;
 
