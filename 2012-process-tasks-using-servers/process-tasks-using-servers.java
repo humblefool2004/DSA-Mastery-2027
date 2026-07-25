4657bfec06e823dfdd4a8540a7fae425093ef8fe
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-26 00:15:12 +0530

    Time: 275 ms (8.54%) | Memory: 170.5 MB (60.76%) - LeetSync

--- 2012-process-tasks-using-servers/process-tasks-using-servers.java
+++ 2012-process-tasks-using-servers/process-tasks-using-servers.java
@@ -1,50 +1,59 @@
 class Solution {
     public int[] assignTasks(int[] servers, int[] tasks) {
+
         int n = tasks.length;
         int m = servers.length;
 
         PriorityQueue<Integer> freePq = new PriorityQueue<>((a, b) -> {
-            if (servers[a] != servers[b]) {
+            if (servers[a] != servers[b])
                 return Integer.compare(servers[a], servers[b]);
-            } else
-                return Integer.compare(a, b);
+            return Integer.compare(a, b);
         });
+
         for (int i = 0; i < m; i++) {
             freePq.add(i);
         }
 
-        //free time, weight, serverid
-        PriorityQueue<int[]> busyPq = new PriorityQueue<>(
-                (a, b) -> {
-                    if (a[0] != b[0])
-                        return Integer.compare(a[0], b[0]);
-                    else if (a[1] != b[1])
-                        return Integer.compare(a[1], b[1]);
-                    else
-                        return Integer.compare(a[2], b[2]);
-                });
+        // {freeTime, weight, serverId}
+        PriorityQueue<long[]> busyPq = new PriorityQueue<>((a, b) -> {
+            if (a[0] != b[0])
+                return Long.compare(a[0], b[0]);
+            if (a[1] != b[1])
+                return Long.compare(a[1], b[1]);
+            return Long.compare(a[2], b[2]);
+        });
 
-        int ans[] = new int[n];
+        int[] ans = new int[n];
 
-        int currentTime = 0;
+        long currentTime = 0;
 
-        for (int i = 0; i < n;i++) {
+        for (int i = 0; i < n; i++) {
 
-            currentTime = Math.max(currentTime, i);
+            currentTime = Math.max(currentTime, (long) i);
 
             while (!busyPq.isEmpty() && busyPq.peek()[0] <= currentTime) {
-                freePq.add(busyPq.poll()[2]);
+                freePq.add((int) busyPq.poll()[2]);
             }
+
             if (freePq.isEmpty()) {
+
                 currentTime = busyPq.peek()[0];
+
                 while (!busyPq.isEmpty() && busyPq.peek()[0] == currentTime) {
-                    freePq.add(busyPq.poll()[2]);
+                    freePq.add((int) busyPq.poll()[2]);
                 }
             }
-            int a=freePq.poll();
-            ans[i] = a;
-            busyPq.add(new int[] { currentTime+tasks[i],servers[a], a });
+
+            int server = freePq.poll();
+            ans[i] = server;
+
+            busyPq.add(new long[]{
+                currentTime + tasks[i],
+                servers[server],
+                server
+            });
         }
+
         return ans;
     }
 }
