5781ac8500ffdeb73898d75c5933848f8551fd48
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-26 11:27:30 +0530

    Time: 8 ms (91.79%) | Memory: 49 MB (75.46%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         if(intervals.length==1) return intervals;
-        Arrays.sort(intervals,(a,b)-> {
-            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
-            return Integer.compare(a[1],b[1]);
-        });
+        Arrays.sort(intervals,(a,b)-> 
+            Integer.compare(a[0],b[0])
+        );
 
         int start=intervals[0][0];
         int end= intervals[0][1];
