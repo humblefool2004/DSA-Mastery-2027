820ae9489d9cc1cd53f72c5ecc53efc01b81909e
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-28 13:05:32 +0530

    Time: 9 ms (91.32%) | Memory: 43.6 MB (56.13%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.java
+++ 686-repeated-string-match/repeated-string-match.java
@@ -1,58 +1,67 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int[] lps= lpsFinder(b);
-        //making sure a>=b;
-        StringBuilder sb=new StringBuilder(a);
-        int numberOfAppends=1;
-        while(sb.length()<b.length()){
+        int[] lps = lpsFinder(b);
+
+        int numberOfAppends = 1;
+        while (numberOfAppends * a.length() < b.length()) {
             numberOfAppends++;
+        }
+
+        StringBuilder sb = new StringBuilder(a.length() * (numberOfAppends + 1));
+        for (int i = 0; i < numberOfAppends + 1; i++) {
             sb.append(a);
         }
 
-        int maxAppendPossible=numberOfAppends+1;
-        
-        int j=0;
-        int len=0;
-        while(numberOfAppends<=maxAppendPossible){
-            while(len<sb.length()){
-                if(sb.charAt(len)==b.charAt(j)){
-                    len++;
-                    j++;
-                    if(j==b.length()){
+        int j = 0;
+        int len = 0;
+        int limit = numberOfAppends * a.length();
+
+        while (len < sb.length()) {
+
+            if (sb.charAt(len) == b.charAt(j)) {
+                len++;
+                j++;
+
+                if (j == b.length()) {
+                    if (len <= limit)
                         return numberOfAppends;
-                    }
-                }else{
-                    if(j!=0){
-                        j=lps[j-1];
-                    }
-                    else{
-                        len++;
-                    }
+                    return numberOfAppends + 1;
                 }
-            }
-            sb.append(a);
-            numberOfAppends++;
-        }
-        return -1;
 
+            } else {
+
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    len++;
+                }
+
+            }
+        }
+
+        return -1;
     }
 
-    private int[] lpsFinder(String s){
-        int[] arr= new int[s.length()];
-        arr[0]=0;
-        int length=0;
-        for(int i=1;i<s.length();){
-            if(s.charAt(i)==s.charAt(length)){
-                arr[i]=  ++length;
+    private int[] lpsFinder(String s) {
+        int[] arr = new int[s.length()];
+        int length = 0;
+
+        for (int i = 1; i < s.length();) {
+
+            if (s.charAt(i) == s.charAt(length)) {
+                arr[i] = ++length;
                 i++;
-            }else{
-                if(length!=0){
-                    length= arr[length-1];
-                }else{
-                    arr[i++]=0;
+            } else {
+
+                if (length != 0) {
+                    length = arr[length - 1];
+                } else {
+                    arr[i++] = 0;
                 }
+
             }
         }
+
         return arr;
     }
 }
