0d3803e91161069c0d9949215e1f4f96d4ae0f9e
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-28 12:42:57 +0530

    Time: 10 ms (90.62%) | Memory: 44 MB (23.41%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int[] lps= lpsFinder(b);
+        //making sure a>=b;
+        StringBuilder sb=new StringBuilder(a);
+        int numberOfAppends=1;
+        while(sb.length()<b.length()){
+            numberOfAppends++;
+            sb.append(a);
+        }
+
+        int maxAppendPossible=2 * numberOfAppends;
+        
+        boolean found=false;
+        int j=0;
+        int len=0;
+        while(numberOfAppends<=maxAppendPossible){
+            while(len<sb.length()){
+                if(sb.charAt(len)==b.charAt(j)){
+                    len++;
+                    j++;
+                    if(j==b.length()){
+                        found=true;
+                        break;
+                    }
+                }else{
+                    if(j!=0){
+                        j=lps[j-1];
+                    }
+                    else{
+                        len++;
+                    }
+                }
+            }
+            if(found) break;
+            sb.append(a);
+            numberOfAppends++;
+        }
+        return found ? numberOfAppends : -1;
+
+    }
+
+    private int[] lpsFinder(String s){
+        int[] arr= new int[s.length()];
+        arr[0]=0;
+        int length=0;
+        for(int i=1;i<s.length();){
+            if(s.charAt(i)==s.charAt(length)){
+                arr[i]=  ++length;
+                i++;
+            }else{
+                if(length!=0){
+                    length= arr[length-1];
+                }else{
+                    arr[i++]=0;
+                }
+            }
+        }
+        return arr;
+    }
+}
