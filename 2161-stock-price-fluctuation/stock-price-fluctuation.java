6aba717fe8e62a008f0de8e9f0d33d486a6382ea
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-29 19:20:45 +0530

    Time: 70 ms (80.54%) | Memory: 131.2 MB (45.86%) - LeetSync

--- 2161-stock-price-fluctuation/stock-price-fluctuation.java
+++ 2161-stock-price-fluctuation/stock-price-fluctuation.java
@@ -1,37 +1,43 @@
 class StockPrice {
-
-    private TreeMap<Integer,Integer> map; //time,price
-    private TreeMap<Integer,Integer> priceFreq; // frequency of prices
-    private int latestTimestamp = 0;
+    private Map<Integer, Integer> timeToPrice;
+    private PriorityQueue<int[]> minHeap;
+    private PriorityQueue<int[]> maxHeap;
+    private int latestTimestamp;
     public StockPrice() {
-        map= new TreeMap<>();
-        priceFreq= new TreeMap<>();
+        timeToPrice= new HashMap<>();
+        minHeap= new PriorityQueue<>((a,b)->
+            Integer.compare(a[0],b[0])
+        );
+        maxHeap= new PriorityQueue<>((a,b)->
+            Integer.compare(b[0],a[0])
+        );
     }
     
     public void update(int timestamp, int price) {
-        if(map.containsKey(timestamp)){
-            int oldPrice = map.get(timestamp);
-            if(priceFreq.get(oldPrice)==1){
-                priceFreq.remove(oldPrice);
-            }else{
-                priceFreq.put(oldPrice,priceFreq.get(oldPrice)-1);
-            }
-        }
-        map.put(timestamp,price);
-        priceFreq.put(price, priceFreq.getOrDefault(price,0)+1);
-        latestTimestamp = Math.max(latestTimestamp, timestamp);
+        int[] arr={price,timestamp};
+        minHeap.offer(arr);
+        maxHeap.offer(arr);
+        timeToPrice.put(timestamp,price);
+        latestTimestamp=Math.max(latestTimestamp,timestamp);
     }
     
     public int current() {
-        return map.get(latestTimestamp);
+        return timeToPrice.get(latestTimestamp);
     }
     
     public int maximum() {
-        return priceFreq.lastKey();
+
+        while(timeToPrice.get(maxHeap.peek()[1]) !=maxHeap.peek()[0]){
+            maxHeap.poll();
+        }
+        return maxHeap.peek()[0];
     }
     
     public int minimum() {
-        return priceFreq.firstKey();
+        while(timeToPrice.get(minHeap.peek()[1]) !=minHeap.peek()[0]){
+            minHeap.poll();
+        }
+        return minHeap.peek()[0];    
     }
 }
 
@@ -43,3 +49,43 @@ class StockPrice {
  * int param_3 = obj.maximum();
  * int param_4 = obj.minimum();
  */
+
+
+
+ //solved using two treemaps
+// class StockPrice {
+
+//     private TreeMap<Integer,Integer> map; //time,price
+//     private TreeMap<Integer,Integer> priceFreq; // frequency of prices
+//     private int latestTimestamp = 0;
+//     public StockPrice() {
+//         map= new TreeMap<>();
+//         priceFreq= new TreeMap<>();
+//     }
+    
+//     public void update(int timestamp, int price) {
+//         if(map.containsKey(timestamp)){
+//             int oldPrice = map.get(timestamp);
+//             if(priceFreq.get(oldPrice)==1){
+//                 priceFreq.remove(oldPrice);
+//             }else{
+//                 priceFreq.put(oldPrice,priceFreq.get(oldPrice)-1);
+//             }
+//         }
+//         map.put(timestamp,price);
+//         priceFreq.put(price, priceFreq.getOrDefault(price,0)+1);
+//         latestTimestamp = Math.max(latestTimestamp, timestamp);
+//     }
+    
+//     public int current() {
+//         return map.get(latestTimestamp);
+//     }
+    
+//     public int maximum() {
+//         return priceFreq.lastKey();
+//     }
+    
+//     public int minimum() {
+//         return priceFreq.firstKey();
+//     }
+// }
