7d3e6982b9000f419d33f121f155ee248b7e7672
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-29 12:41:41 +0530

    Time: 62 ms (37.96%) | Memory: 47.7 MB (50.15%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -7,6 +7,35 @@ class Solution {
             return false;
         int target = totalSum / 2;
 
+        boolean dp[][] = new boolean[nums.length][target + 1];
+        for (int i = 0; i < nums.length; i++)
+            dp[i][0] = true;
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
+        for (int idx = 1; idx < nums.length; idx++) {
+            for (int sum = 1; sum < dp[0].length; sum++) {
+                dp[idx][sum] = dp[idx - 1][sum];
+
+                if (sum >= nums[idx]) {
+                    dp[idx][sum] |= dp[idx - 1][sum - nums[idx]];
+                }
+            }
+        }
+        return dp[nums.length - 1][target];
+    }
+}
+
+//recursion+memorization
+/*
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums)
+            totalSum += num;
+        if (totalSum % 2 == 1)
+            return false;
+        int target = totalSum / 2;
+
         Boolean dp[][] = new Boolean[nums.length][target + 1]; //dp creatinon
 
         return check(nums, nums.length - 1, 0, target, dp);
@@ -25,3 +54,4 @@ class Solution {
                 || check(nums, idx - 1, sum, target, dp));
     }
 }
+*/
