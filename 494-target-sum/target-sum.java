462e8c4ee8ee7aa81d7dbfee4041acd53b88eb4c
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-30 12:43:03 +0530

    Time: 6 ms (72.49%) | Memory: 46.8 MB (23.00%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,17 +1,21 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return search(nums.length-1,nums,target,0);
+
+        Integer[][] dp= new Integer[nums.length][2002];
+
+        return search(nums.length-1,nums,target,0,dp);
     }
 
     //search returns the number of paths.
-    private int search(int idx,int[] nums, int target,int sum){
+    private int search(int idx,int[] nums, int target,int sum,Integer[][] dp){
         if(idx<0){
             if(sum==target) return 1;
             return 0;
         }
+        if(dp[idx][sum+1000]!=null) return dp[idx][sum+1000];
 
-        int plus=search(idx-1,nums,target,sum+nums[idx]);
-        int minus=search(idx-1,nums,target,sum-nums[idx]);
-        return plus+minus;
+        int plus=search(idx-1,nums,target,sum+nums[idx],dp);
+        int minus=search(idx-1,nums,target,sum-nums[idx],dp);
+        return dp[idx][sum+1000]=plus+minus;
     }
 }
