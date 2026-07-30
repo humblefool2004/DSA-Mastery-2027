d44669dc366b479a5dca0fbac8af84938863f1c8
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-30 11:49:36 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (99.63%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -4,8 +4,6 @@ class Solution {
         if(nums.length==2) return Math.max(nums[0],nums[1]);
         if(nums.length==3) return Math.max(nums[0],Math.max(nums[1],nums[2]));
 
-        
-
         int prev2=0;
         int prev1=nums[1];
         for(int i=2;i<nums.length;i++){
@@ -14,10 +12,8 @@ class Solution {
             prev1=curr;
         }
         int max= prev1;
-
         prev2=nums[0];
         prev1=Math.max(nums[0], nums[1]);
-
         for(int i=2;i<nums.length-1;i++){
             int curr= Math.max(prev1,prev2+nums[i]);
             prev2=prev1;
