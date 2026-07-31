e709d62dd2d6cba47f3c65b7606def852261972c
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-31 10:47:49 +0530

    Time: 18 ms (78.01%) | Memory: 48.4 MB (32.98%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,30 +1,36 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack= new Stack<>();
-        char prev='+';
-        for(int i=0;i<s.length();i++){
+        Stack<Integer> stack = new Stack<>();
+        char prev = '+';
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch==' ') continue;
-            if(Character.isDigit(ch)){
-                int start=i;
-                while(i+1<s.length() && Character.isDigit(s.charAt(i+1))){
+            if (ch == ' ')
+                continue;
+            if (Character.isDigit(ch)) {
+                int a = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    a = a * 10 + (s.charAt(i) - '0');
                     i++;
                 }
-                int a=Integer.parseInt(s.substring(start,i+1));
-                if(prev=='+') stack.push(a);
-                else if(prev=='-') stack.push(-a);
+                i--;
+                if (prev == '+')
+                    stack.push(a);
+                else if (prev == '-')
+                    stack.push(-a);
                 else {
-                    int b= stack.pop();
-                    if(prev=='*') stack.push(a*b);
-                    else stack.push(b/a);
+                    int b = stack.pop();
+                    if (prev == '*')
+                        stack.push(a * b);
+                    else
+                        stack.push(b / a);
                 }
-            }else{
-                prev= ch;
+            } else {
+                prev = ch;
             }
         }
-        int ans=0;
-        while(!stack.isEmpty()){
-            ans+=stack.pop();
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
         }
         return ans;
     }
