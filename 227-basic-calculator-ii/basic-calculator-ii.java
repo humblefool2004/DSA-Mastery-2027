6e42a0c5913f5a448d5de10a37bc235c01150ade
Saheel Mohanta <humblefool2004@gmail.com>
2026-07-31 10:44:55 +0530

    Time: 31 ms (10.97%) | Memory: 49.5 MB (7.48%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,99 +1,31 @@
 class Solution {
     public int calculate(String s) {
-        int result=0; //final answer;
-        char op = '+'; //last operator
-        int lastNum = 0; //lastNumber   
-        int currNum=0; //current Number
-        for (int i = 0; i < s.length(); i++) {
+        Stack<Integer> stack= new Stack<>();
+        char prev='+';
+        for(int i=0;i<s.length();i++){
             char ch = s.charAt(i);
-            if (Character.isDigit(ch)) {
-                currNum = currNum * 10 + (ch - '0');
-            }
-            if((!Character.isDigit(ch) && ch!=' ') || (i == s.length() - 1) ){
-
-                if (op == '+') {
-                    result+=lastNum;
-                    lastNum=currNum;
-                } else if (op == '-') {
-                    result+=lastNum;
-                    lastNum= -currNum;
-                } else if (op == '*') {
-                    lastNum= lastNum*currNum;
-                } else if (op == '/') {
-                    lastNum=lastNum/currNum;
+            if(ch==' ') continue;
+            if(Character.isDigit(ch)){
+                int start=i;
+                while(i+1<s.length() && Character.isDigit(s.charAt(i+1))){
+                    i++;
                 }
-                op = ch;
-                currNum = 0;
+                int a=Integer.parseInt(s.substring(start,i+1));
+                if(prev=='+') stack.push(a);
+                else if(prev=='-') stack.push(-a);
+                else {
+                    int b= stack.pop();
+                    if(prev=='*') stack.push(a*b);
+                    else stack.push(b/a);
+                }
+            }else{
+                prev= ch;
             }
         }
-
-        return result+lastNum;
+        int ans=0;
+        while(!stack.isEmpty()){
+            ans+=stack.pop();
+        }
+        return ans;
     }
 }
-
-//Overly Engineered approach
-// class Solution {
-//     public int calculate(String s) {
-//         Stack<Character> ch = new Stack<>();
-//         Stack<Integer> st = new Stack<>();
-//         s = s.replaceAll(" ", "");
-
-//         for (int i = 0; i < s.length(); i++) {
-//             char ss = s.charAt(i);
-//             if (ss == '+' || ss == '-' || ss == '*' || ss == '/') {
-//                 if (ch.isEmpty())
-//                     ch.push(ss);
-//                 else if (ss == '*' || ss == '/') {
-//                     while (!ch.isEmpty() && (ch.peek() == '*' || ch.peek() == '/')) {
-//                         char op = ch.pop();
-//                         int b = st.pop();
-//                         int a = st.pop();
-//                         int res = switch (op) {
-//                             case '*' -> a * b;
-//                             case '/' -> a / b;
-//                             default -> 0;
-//                         };
-//                         st.push(res);
-//                     }
-//                     ch.push(ss);
-//                 } else {
-//                     while (!ch.isEmpty()) {
-//                         char op = ch.pop();
-//                         int b = st.pop();
-//                         int a = st.pop();
-//                         int res = switch (op) {
-//                             case '*' -> a * b;
-//                             case '/' -> a / b;
-//                             case '-' -> a - b;
-//                             case '+' -> a + b;
-//                             default -> 0;
-//                         };
-//                         st.push(res);
-//                     }
-//                     ch.push(ss);
-//                 }
-//             } else {
-//                 int start = i;
-//                 while (i < s.length() - 1 && Character.isDigit(s.charAt(i + 1))) {
-//                     i++;
-//                 }
-//                 String number = s.substring(start, i + 1);
-//                 st.push(Integer.parseInt(number));
-//             }
-//         }
-//         while (!ch.isEmpty()) {
-//             char op = ch.pop();
-//             int b = st.pop();
-//             int a = st.pop();
-//             int res = switch (op) {
-//                 case '*' -> a * b;
-//                 case '/' -> a / b;
-//                 case '-' -> a - b;
-//                 case '+' -> a + b;
-//                 default -> 0;
-//             };
-//             st.push(res);
-//         }
-//         return st.peek();
-//     }
-// }
