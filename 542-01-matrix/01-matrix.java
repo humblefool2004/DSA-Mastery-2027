dab5a1c2cdeb47bae086287dd5eb0935f13eea38
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-03 22:45:38 +0530

    Time: 12 ms (93.14%) | Memory: 48 MB (98.47%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,65 +1,48 @@
 class Solution {
 
-    private boolean[][] visited;
-
     public int[][] updateMatrix(int[][] mat) {
 
         int m = mat.length;
         int n = mat[0].length;
 
-        visited = new boolean[m][n];
         Queue<int[]> queue = new ArrayDeque<>();
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
                     queue.offer(new int[] { i, j });
-                    visited[i][j] = true;
+                } else {
+                    mat[i][j] = -1; // mark unvisited
                 }
             }
         }
 
-        int k = 0;
+        int[][] dir = {
+            {-1, 0},
+            {1, 0},
+            {0, -1},
+            {0, 1}
+        };
 
         while (!queue.isEmpty()) {
 
-            int size = queue.size();
+            int[] cell = queue.poll();
+            int r = cell[0];
+            int c = cell[1];
 
-            while (size-- > 0) {
+            for (int[] d : dir) {
 
-                int[] cell = queue.poll();
-                int r = cell[0];
-                int c = cell[1];
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                if (k > 0)
-                    mat[r][c] = k;
+                if (nr >= 0 && nr < m &&
+                    nc >= 0 && nc < n &&
+                    mat[nr][nc] == -1) {
 
-                // Up
-                if (r > 0 && !visited[r - 1][c]) {
-                    visited[r - 1][c] = true;
-                    queue.offer(new int[] { r - 1, c });
-                }
-
-                // Down
-                if (r < m - 1 && !visited[r + 1][c]) {
-                    visited[r + 1][c] = true;
-                    queue.offer(new int[] { r + 1, c });
-                }
-
-                // Left
-                if (c > 0 && !visited[r][c - 1]) {
-                    visited[r][c - 1] = true;
-                    queue.offer(new int[] { r, c - 1 });
-                }
-
-                // Right
-                if (c < n - 1 && !visited[r][c + 1]) {
-                    visited[r][c + 1] = true;
-                    queue.offer(new int[] { r, c + 1 });
+                    mat[nr][nc] = mat[r][c] + 1;
+                    queue.offer(new int[] { nr, nc });
                 }
             }
-
-            k++;
         }
 
         return mat;
