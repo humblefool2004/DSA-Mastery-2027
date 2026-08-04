b0b0e39bfca85418b98fd4a3d753d76f5c02ad78
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-04 21:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.31%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.java
+++ 233-number-of-digit-one/number-of-digit-one.java
@@ -1,16 +1,21 @@
 class Solution {
 
-    private Integer[][][] memo;
+    private int[][][] memo;
 
     public int countDigitOne(int n) {
-        memo=new Integer[11][2][11];
+        memo=new int[11][2][11];
+        for(int i=0;i<11;i++){
+            for(int j=0;j<2;j++){
+                Arrays.fill(memo[i][j],-1);
+            }
+        }
         String st= String.valueOf(n);
         return solve(st,0,1,0);
     }
 
     private int solve(String st,int idx,int bool,int countOfOne){
         if(idx==st.length()) return countOfOne;
-        if(memo[idx][bool][countOfOne] !=null) return memo[idx][bool][countOfOne];
+        if(memo[idx][bool][countOfOne] !=-1) return memo[idx][bool][countOfOne];
         int ub= (bool==1) ? st.charAt(idx)-'0' : 9;
         int res=0;
         for(int i=0;i<=ub;i++){
