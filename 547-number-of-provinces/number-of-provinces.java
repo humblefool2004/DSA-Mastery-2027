43ac5a4f7e93c5875b2737875df4861f760b13e9
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-05 23:38:39 +0530

    Time: 1 ms (86.50%) | Memory: 47.1 MB (88.74%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.java
@@ -0,0 +1,46 @@
+class Solution {
+    private class DisjointSet{
+        private int[] size;
+        private int[] ultP;
+
+        public DisjointSet(int n){
+            size=new int[n+1];
+            ultP= new int[n+1];
+            for(int i=1;i<=n;i++){
+                size[i]=1;
+                ultP[i]=i;
+            }
+        }
+        public int findParent(int u){
+            if(u==ultP[u]) return u;
+            return ultP[u]= findParent(ultP[u]);
+        }
+        public void union(int u,int v){
+            int uP=findParent(u);
+            int vP= findParent(v);
+            if(uP==vP) return;
+            if(size[uP]<size[vP]){
+                ultP[uP]=vP;
+                size[vP]+=size[uP];
+            }else{
+                ultP[vP]=uP;
+                size[uP]+=size[vP];
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n= isConnected.length;
+        DisjointSet ds= new DisjointSet(isConnected.length);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1) ds.union(i+1,j+1);
+            }
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(ds.findParent(i)==i) ans++;
+        }
+        return ans;
+    }
+}
