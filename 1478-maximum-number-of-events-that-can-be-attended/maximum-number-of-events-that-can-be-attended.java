bb8176d7b7cf041b4bbc3455c7c2e6af630410d1
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-05 12:42:19 +0530

    Time: 71 ms (31.66%) | Memory: 103.2 MB (47.01%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,34 +1,42 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events,(a,b)->{
-            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
-            return Integer.compare(a[1],b[1]);
-        });
-        
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->{
-            if(a[1]!=b[1]) return Integer.compare(a[1],b[1]);
-            return Integer.compare(a[0],b[0]);
-        });
-        
-        int eventCount=0;
-        int day= events[0][0];
-        int i=0;
-        while(i<events.length || !pq.isEmpty()){
-            while(i<events.length && events[i][0]==day){
-                pq.offer(events[i++]);
-            }
-            
-            while(!pq.isEmpty() && pq.peek()[1]<day) pq.poll();
 
-            if(pq.isEmpty() ){
-                if(i<events.length) day= events[i][0];
-            }else{
+        Arrays.sort(events, (a, b) -> {
+            if (a[0] != b[0])
+                return Integer.compare(a[0], b[0]);
+            return Integer.compare(a[1], b[1]);
+        });
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int eventCount = 0;
+        int day = events[0][0];
+        int i = 0;
+
+        while (i < events.length || !pq.isEmpty()) {
+
+            // Add all events starting today
+            while (i < events.length && events[i][0] == day) {
+                pq.offer(events[i][1]);
+                i++;
+            }
+
+            // Remove expired events
+            while (!pq.isEmpty() && pq.peek() < day) {
+                pq.poll();
+            }
+
+            // Attend one event
+            if (!pq.isEmpty()) {
                 pq.poll();
                 eventCount++;
                 day++;
+            } else if (i < events.length) {
+                // Jump to the next event's start day
+                day = events[i][0];
             }
-            
         }
+
         return eventCount;
     }
 }
