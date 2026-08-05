229564cdd48e18bb32005a7534d5b94c18990c22
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-05 12:40:03 +0530

    Time: 66 ms (89.77%) | Memory: 100.2 MB (98.46%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,20 +1,24 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events,(a,b)->
-      Integer.compare(a[0],b[0])
-        );
+        Arrays.sort(events,(a,b)->{
+            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
+            return Integer.compare(a[1],b[1]);
+        });
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->{
+            if(a[1]!=b[1]) return Integer.compare(a[1],b[1]);
+            return Integer.compare(a[0],b[0]);
+        });
         
         int eventCount=0;
         int day= events[0][0];
         int i=0;
         while(i<events.length || !pq.isEmpty()){
             while(i<events.length && events[i][0]==day){
-                pq.offer(events[i++][1]);
+                pq.offer(events[i++]);
             }
             
-            while(!pq.isEmpty() && pq.peek()<day) pq.poll();
+            while(!pq.isEmpty() && pq.peek()[1]<day) pq.poll();
 
             if(pq.isEmpty() ){
                 if(i<events.length) day= events[i][0];
