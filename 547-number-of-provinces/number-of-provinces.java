ae1422ae9f63e078686393512ce46ac98f20bf17
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-05 23:39:35 +0530

    Time: 1 ms (86.50%) | Memory: 47.3 MB (44.43%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -31,7 +31,7 @@ class Solution {
 
     public int findCircleNum(int[][] isConnected) {
         int n= isConnected.length;
-        DisjointSet ds= new DisjointSet(isConnected.length);
+        DisjointSet ds= new DisjointSet(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(isConnected[i][j]==1) ds.union(i+1,j+1);
