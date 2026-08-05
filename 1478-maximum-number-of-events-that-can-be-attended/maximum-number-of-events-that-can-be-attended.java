3c8206c61431877df77b3a917211167cc7410ed6
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-05 12:38:13 +0530

    Time: 71 ms (31.66%) | Memory: 103.4 MB (30.79%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -5,20 +5,17 @@ class Solution {
             return Integer.compare(a[1],b[1]);
         });
         
-        PriorityQueue<int[]> pq= new PriorityQueue<>((a,b)->{
-            if(a[1]!=b[1]) return Integer.compare(a[1],b[1]);
-            return Integer.compare(a[0],b[0]);
-        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         int eventCount=0;
         int day= events[0][0];
         int i=0;
         while(i<events.length || !pq.isEmpty()){
             while(i<events.length && events[i][0]==day){
-                pq.offer(events[i++]);
+                pq.offer(events[i++][1]);
             }
             
-            while(!pq.isEmpty() && pq.peek()[1]<day) pq.poll();
+            while(!pq.isEmpty() && pq.peek()<day) pq.poll();
 
             if(pq.isEmpty() ){
                 if(i<events.length) day= events[i][0];
