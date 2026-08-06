ad1e7907f72e2955f4b75a916ea8ec3d7bbda217
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-06 14:09:48 +0530

    Time: 73 ms (90.57%) | Memory: 48.5 MB (73.02%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -1,57 +1,61 @@
 class Solution {
     public int openLock(String[] deadends, String target) {
-        if(target.equals("0000")) return 0;
-        Set<String> set= new HashSet<>();
+        if (target.equals("0000"))
+            return 0;
+        Set<String> set = new HashSet<>();
         set.addAll(Arrays.asList(deadends)); //works as a visited set too.
-        if(set.contains("0000")) return -1;
+        if (set.contains("0000"))
+            return -1;
 
-        Queue<String> queue= new ArrayDeque<>();
+        Queue<String> queue = new ArrayDeque<>();
 
         queue.offer("0000");
         set.add("0000");
-        int level=0;
-        while(!queue.isEmpty()){
-            int n=queue.size();
-            for(int i=0;i<n;i++){
-                char[] arr = queue.poll().toCharArray();
-                if(target.equals(String.valueOf(arr))) return level;
-                for(int  j=0;j<4;j++){
-                    if(arr[j] =='0'){
-                        arr[j]='9';
-                        String next=String.valueOf(arr);
-                        if(!set.contains(next)){
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                String curr = queue.poll();
+                if (target.equals(curr))
+                    return level;
+                char[] arr = curr.toCharArray();
+                for (int j = 0; j < 4; j++) {
+                    if (arr[j] == '0') {
+                        arr[j] = '9';
+                        String next = String.valueOf(arr);
+                        if (!set.contains(next)) {
                             queue.offer(next);
                             set.add(next);
                         }
-                        arr[j]='0';
-                    }else{
-                        arr[j]= (char)(arr[j]-1);
-                        String next=String.valueOf(arr);
-                        if(!set.contains(next)){
+                        arr[j] = '0';
+                    } else {
+                        arr[j] = (char) (arr[j] - 1);
+                        String next = String.valueOf(arr);
+                        if (!set.contains(next)) {
                             queue.offer(next);
                             set.add(next);
                         }
-                        arr[j]= (char)(arr[j]+1);
+                        arr[j] = (char) (arr[j] + 1);
                     }
 
-                    if(arr[j] =='9'){
-                        arr[j]='0';
-                        String next=String.valueOf(arr);
-                        if(!set.contains(next)){
+                    if (arr[j] == '9') {
+                        arr[j] = '0';
+                        String next = String.valueOf(arr);
+                        if (!set.contains(next)) {
                             queue.offer(next);
                             set.add(next);
                         }
-                        arr[j]='9';
-                    }else{
-                        arr[j]= (char)(arr[j]+1);
-                        String next=String.valueOf(arr);
-                        if(!set.contains(next)){
+                        arr[j] = '9';
+                    } else {
+                        arr[j] = (char) (arr[j] + 1);
+                        String next = String.valueOf(arr);
+                        if (!set.contains(next)) {
                             queue.offer(next);
                             set.add(next);
                         }
-                        arr[j]= (char)(arr[j]-1);
+                        arr[j] = (char) (arr[j] - 1);
                     }
-            
+
                 }
             }
             level++;
