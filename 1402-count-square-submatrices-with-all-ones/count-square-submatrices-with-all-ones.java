78bebc5c6d41d44894327a4061604721cddc4fd2
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-07 01:18:53 +0530

    Time: 10 ms (8.87%) | Memory: 61.2 MB (97.36%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -1,29 +1,32 @@
 class Solution {
-
     public int countSquares(int[][] matrix) {
+        int m=matrix.length;
+        int n= matrix[0].length;
 
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] dp = new int[m][n];
+        int dp[][]= new int[m][n];
 
-        int ans = 0;
-
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-
-                if (matrix[i][j] == 0)
-                    dp[i][j] = 0;
-                else if (i == m - 1 || j == n - 1)
-                    dp[i][j] = 1;
-                else
-                    dp[i][j] = 1 + Math.min(dp[i][j + 1],
-                            Math.min(dp[i + 1][j],
-                                    dp[i + 1][j + 1]));
-
-                ans += dp[i][j];
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                ans+=solve(matrix,i,j,dp);
             }
         }
-
         return ans;
+    }
+
+    //solve function returns the size of the largest possible square with all 1s from r,c;
+    private int solve(int[][] matrix,int r, int c,int[][] dp){
+        if(r==matrix.length || c==matrix[0].length) return 0;
+        if(matrix[r][c]==0) return 0;
+
+        if(dp[r][c]!=0) return dp[r][c];
+        ////very very imporant part of this recursion.***
+        //it tells valid squares on the position right, down,and diag. 
+        int right=solve(matrix,r,c+1,dp);
+        int down= solve(matrix,r+1,c,dp);
+        int diag= solve(matrix,r+1,c+1,dp);
+
+        //here 1 is added because atleast square of size 1 is possible
+        return dp[r][c]=1+ Math.min(right,Math.min(down,diag));
     }
 }
