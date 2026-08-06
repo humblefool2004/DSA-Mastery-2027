bed42914d0d410dff5ac20741a4405a7703acf6e
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-06 20:34:12 +0530

    Time: 3 ms (26.42%) | Memory: 47.7 MB (76.12%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,59 +1,59 @@
-//multisource dfs
+//multisource bfs
 class Solution {
     public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        boolean[][] visited= new boolean[m][n];
-        Queue<int[]> queue= new ArrayDeque<>();
-        for(int i=0;i<m;i++){
-            if(!visited[i][0] && board[i][0]=='O'){
-                queue.offer(new int[]{i,0});
-                visited[i][0]=true;
-            } 
-            if(!visited[i][n-1] && board[i][n-1]=='O'){
-                queue.offer(new int[]{i,n-1});
-                visited[i][n-1]=true;
+        int m = board.length;
+        int n = board[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] != '#' && board[i][0] == 'O') {
+                queue.offer(new int[] { i, 0 });
+                board[i][0] = '#';
+            }
+            if (board[i][n - 1] != '#' && board[i][n - 1] == 'O') {
+                queue.offer(new int[] { i, n - 1 });
+                board[i][n - 1] = '#';
             }
         }
-        for(int j=0;j<n;j++){
-            if(!visited[0][j] && board[0][j]=='O'){
-                queue.offer(new int[]{0,j});
-                visited[0][j]=true;
-            } 
-            if(!visited[m-1][j] && board[m-1][j]=='O'){
-                queue.offer(new int[]{m-1,j});
-                visited[m-1][j]=true;
-            } 
+        for (int j = 0; j < n; j++) {
+            if (board[0][j]!='#' && board[0][j] == 'O') {
+                queue.offer(new int[] { 0, j });
+                board[0][j] ='#';
+            }
+            if (board[m - 1][j]!='#' && board[m - 1][j] == 'O') {
+                queue.offer(new int[] { m - 1, j });
+                board[m - 1][j] = '#';
+            }
         }
 
-        while(!queue.isEmpty()){
-            int k= queue.size();
-            for(int i=0;i<k;i++){
-                int[] a= queue.poll();
-                if(a[0]!=0 && !visited[a[0]-1][a[1]] && board[a[0]-1][a[1]]=='O'){
-                    queue.offer(new int[]{a[0]-1,a[1]});
-                    visited[a[0]-1][a[1]]=true;
-                }
-                if(a[0]!=m-1 && !visited[a[0]+1][a[1]] && board[a[0]+1][a[1]]=='O'){
-                    queue.offer(new int[]{a[0]+1,a[1]});
-                    visited[a[0]+1][a[1]]=true;
-                }
+        while (!queue.isEmpty()) {
+            int[] a = queue.poll();
+            if (a[0] != 0 && board[a[0] - 1][a[1]]!='#' && board[a[0] - 1][a[1]] == 'O') {
+                queue.offer(new int[] { a[0] - 1, a[1] });
+                board[a[0] - 1][a[1]] ='#';
+            }
+            if (a[0] != m - 1 && board[a[0] + 1][a[1]]!='#' && board[a[0] + 1][a[1]] == 'O') {
+                queue.offer(new int[] { a[0] + 1, a[1] });
+                board[a[0] + 1][a[1]] = '#';
+            }
 
-                if(a[1]!=0 && !visited[a[0]][a[1]-1] && board[a[0]][a[1]-1]=='O'){
-                    queue.offer(new int[]{a[0],a[1]-1});
-                    visited[a[0]][a[1]-1]=true;
-                }
+            if (a[1] != 0 && board[a[0]][a[1] - 1]!='#' && board[a[0]][a[1] - 1] == 'O') {
+                queue.offer(new int[] { a[0], a[1] - 1 });
+                board[a[0]][a[1] - 1] = '#';
+            }
 
-                if(a[1]!=n-1 && !visited[a[0]][a[1]+1] && board[a[0]][a[1]+1]=='O'){
-                    queue.offer(new int[]{a[0],a[1]+1});
-                    visited[a[0]][a[1]+1]=true;
+            if (a[1] != n - 1 && board[a[0]][a[1] + 1]!='#' && board[a[0]][a[1] + 1] == 'O') {
+                queue.offer(new int[] { a[0], a[1] + 1 });
+                board[a[0]][a[1] + 1] = '#';
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j]!='#' && board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }else if(board[i][j]=='#'){
+                    board[i][j]='O';
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(!visited[i][j]) board[i][j]='X';
-            }
-        }   
     }
 }
