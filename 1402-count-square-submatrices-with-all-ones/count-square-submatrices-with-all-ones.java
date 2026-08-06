63ec7b966670ad1eea8b909fe0eeaef2bb6e911d
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-07 01:33:35 +0530

    Time: 5 ms (99.57%) | Memory: 61.8 MB (44.05%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -2,31 +2,62 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int m=matrix.length;
         int n= matrix[0].length;
-
         int dp[][]= new int[m][n];
-
+        //initializing last row and last column of the dp
         int ans=0;
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans+=solve(matrix,i,j,dp);
+            ans+= dp[i][n-1]=matrix[i][n-1];
+        }
+        for(int j=0;j<n-1;j++){
+            ans+= dp[m-1][j]= matrix[m-1][j];
+        }
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                if(matrix[i][j]==1){  
+                    ans+= dp[i][j]= 1+ Math.min( dp[i][j+1],
+                        Math.min(dp[i+1][j],dp[i+1][j+1])
+                    );
+                }
             }
         }
         return ans;
     }
-
-    //solve function returns the size of the largest possible square with all 1s from r,c;
-    private int solve(int[][] matrix,int r, int c,int[][] dp){
-        if(r==matrix.length || c==matrix[0].length) return 0;
-        if(matrix[r][c]==0) return 0;
-
-        if(dp[r][c]!=0) return dp[r][c];
-        ////very very imporant part of this recursion.***
-        //it tells valid squares on the position right, down,and diag. 
-        int right=solve(matrix,r,c+1,dp);
-        int down= solve(matrix,r+1,c,dp);
-        int diag= solve(matrix,r+1,c+1,dp);
-
-        //here 1 is added because atleast square of size 1 is possible
-        return dp[r][c]=1+ Math.min(right,Math.min(down,diag));
-    }
 }
+
+
+
+
+//memo+recursion
+// class Solution {
+//     public int countSquares(int[][] matrix) {
+//         int m=matrix.length;
+//         int n= matrix[0].length;
+
+//         int dp[][]= new int[m][n];
+
+//         int ans=0;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 ans+=solve(matrix,i,j,dp);
+//             }
+//         }
+//         return ans;
+//     }
+
+//     //solve function returns the size of the largest possible square with all 1s from r,c;
+//     private int solve(int[][] matrix,int r, int c,int[][] dp){
+//         if(r==matrix.length || c==matrix[0].length) return 0;
+//         if(matrix[r][c]==0) return 0;
+
+//         if(dp[r][c]!=0) return dp[r][c];
+//         ////very very imporant part of this recursion.***
+//         //it tells valid squares on the position right, down,and diag. 
+//         int right=solve(matrix,r,c+1,dp);
+//         int down= solve(matrix,r+1,c,dp);
+//         int diag= solve(matrix,r+1,c+1,dp);
+
+//         //here 1 is added because atleast square of size 1 is possible
+//         return dp[r][c]=1+ Math.min(right,Math.min(down,diag));
+//     }
+// }
