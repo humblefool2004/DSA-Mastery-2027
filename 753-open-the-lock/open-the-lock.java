a4bd662ba4b615a8ca586e50e6902ee007803d23
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-06 14:00:41 +0530

    Time: 114 ms (26.98%) | Memory: 48.7 MB (64.73%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        if(target.equals("0000")) return 0;
+        Set<String> set= new HashSet<>();
+        set.addAll(Arrays.asList(deadends)); //works as a visited set too.
+        if(set.contains("0000")) return -1;
+
+        Queue<String> queue= new ArrayDeque<>();
+
+        queue.offer("0000");
+        set.add("0000");
+        int level=0;
+        while(!queue.isEmpty()){
+            int n=queue.size();
+            for(int i=0;i<n;i++){
+                StringBuilder sb=new StringBuilder(queue.poll());
+                if(target.equals(sb.toString())) return level;
+                //combinations.
+                for(int  j=0;j<4;j++){
+                    //-1
+                    if(sb.charAt(j) =='0'){
+                        sb.setCharAt(j,'9');
+                        if(!set.contains(sb.toString())){
+                            queue.offer(sb.toString());
+                            set.add(sb.toString());
+                        }
+                            
+                        sb.setCharAt(j,'0');
+                    }else{
+                        sb.setCharAt(j,(char)(sb.charAt(j)-1));
+                        if(!set.contains(sb.toString())){
+                            queue.offer(sb.toString());
+                            set.add(sb.toString());
+                        }
+                        sb.setCharAt(j,(char)(sb.charAt(j)+1));
+                    }
+
+                    //+1
+                    if(sb.charAt(j) =='9'){
+                        sb.setCharAt(j,'0');
+                        if(!set.contains(sb.toString())){
+                            queue.offer(sb.toString());
+                            set.add(sb.toString());
+                        }
+                        sb.setCharAt(j,'9');
+                    }else{
+                        sb.setCharAt(j,(char)(sb.charAt(j)+1));
+                        if(!set.contains(sb.toString())){
+                            queue.offer(sb.toString());
+                            set.add(sb.toString());
+                        }
+                        sb.setCharAt(j,(char)(sb.charAt(j)-1));
+                    }
+                }
+            }
+            level++;
+        }
+
+        return -1;
+    }
+}
