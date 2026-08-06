5b2f3de9a3303029df74966fd8e45a3bf494f074
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-06 20:39:23 +0530

    Time: 2 ms (72.60%) | Memory: 47.7 MB (58.26%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -5,21 +5,21 @@ class Solution {
         int n = board[0].length;
         Queue<int[]> queue = new ArrayDeque<>();
         for (int i = 0; i < m; i++) {
-            if (board[i][0] != '#' && board[i][0] == 'O') {
+            if (board[i][0] == 'O') {
                 queue.offer(new int[] { i, 0 });
                 board[i][0] = '#';
             }
-            if (board[i][n - 1] != '#' && board[i][n - 1] == 'O') {
+            if (board[i][n - 1] == 'O') {
                 queue.offer(new int[] { i, n - 1 });
                 board[i][n - 1] = '#';
             }
         }
         for (int j = 0; j < n; j++) {
-            if (board[0][j]!='#' && board[0][j] == 'O') {
+            if (board[0][j] == 'O') {
                 queue.offer(new int[] { 0, j });
                 board[0][j] ='#';
             }
-            if (board[m - 1][j]!='#' && board[m - 1][j] == 'O') {
+            if (board[m - 1][j] == 'O') {
                 queue.offer(new int[] { m - 1, j });
                 board[m - 1][j] = '#';
             }
@@ -27,32 +27,33 @@ class Solution {
 
         while (!queue.isEmpty()) {
             int[] a = queue.poll();
-            if (a[0] != 0 && board[a[0] - 1][a[1]]!='#' && board[a[0] - 1][a[1]] == 'O') {
-                queue.offer(new int[] { a[0] - 1, a[1] });
-                board[a[0] - 1][a[1]] ='#';
+            int r=a[0];
+            int c= a[1];
+            if (r != 0 && board[r - 1][c] == 'O') {
+                queue.offer(new int[] { r - 1, c });
+                board[r - 1][c] ='#';
             }
-            if (a[0] != m - 1 && board[a[0] + 1][a[1]]!='#' && board[a[0] + 1][a[1]] == 'O') {
-                queue.offer(new int[] { a[0] + 1, a[1] });
-                board[a[0] + 1][a[1]] = '#';
-            }
-
-            if (a[1] != 0 && board[a[0]][a[1] - 1]!='#' && board[a[0]][a[1] - 1] == 'O') {
-                queue.offer(new int[] { a[0], a[1] - 1 });
-                board[a[0]][a[1] - 1] = '#';
+            if (r != m - 1 && board[r + 1][c] == 'O') {
+                queue.offer(new int[] { r + 1, c });
+                board[r + 1][c] = '#';
             }
 
-            if (a[1] != n - 1 && board[a[0]][a[1] + 1]!='#' && board[a[0]][a[1] + 1] == 'O') {
-                queue.offer(new int[] { a[0], a[1] + 1 });
-                board[a[0]][a[1] + 1] = '#';
+            if (c != 0 && board[r][c - 1] == 'O') {
+                queue.offer(new int[] { r, c - 1 });
+                board[r][c - 1] = '#';
+            }
+
+            if (c != n - 1 && board[r][c + 1] == 'O') {
+                queue.offer(new int[] { r, c + 1 });
+                board[r][c + 1] = '#';
             }
         }
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j]!='#' && board[i][j] == 'O') {
+                if (board[i][j] == 'O') 
                     board[i][j] = 'X';
-                }else if(board[i][j]=='#'){
+                else if(board[i][j]=='#')
                     board[i][j]='O';
-                }
             }
         }
     }
