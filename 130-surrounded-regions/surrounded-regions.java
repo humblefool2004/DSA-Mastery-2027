3100ece4824f43e1d55d42fab57e39e2f79c6205
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-06 20:42:41 +0530

    Time: 1 ms (99.82%) | Memory: 47.8 MB (39.51%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,60 +1,53 @@
-//multisource bfs
 class Solution {
+
     public void solve(char[][] board) {
+
         int m = board.length;
         int n = board[0].length;
-        Queue<int[]> queue = new ArrayDeque<>();
+
+        // Left & Right boundaries
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') {
-                queue.offer(new int[] { i, 0 });
-                board[i][0] = '#';
-            }
-            if (board[i][n - 1] == 'O') {
-                queue.offer(new int[] { i, n - 1 });
-                board[i][n - 1] = '#';
-            }
+            if (board[i][0] == 'O')
+                dfs(i, 0, board);
+
+            if (board[i][n - 1] == 'O')
+                dfs(i, n - 1, board);
         }
+
+        // Top & Bottom boundaries
         for (int j = 0; j < n; j++) {
-            if (board[0][j] == 'O') {
-                queue.offer(new int[] { 0, j });
-                board[0][j] ='#';
-            }
-            if (board[m - 1][j] == 'O') {
-                queue.offer(new int[] { m - 1, j });
-                board[m - 1][j] = '#';
-            }
+            if (board[0][j] == 'O')
+                dfs(0, j, board);
+
+            if (board[m - 1][j] == 'O')
+                dfs(m - 1, j, board);
         }
 
-        while (!queue.isEmpty()) {
-            int[] a = queue.poll();
-            int r=a[0];
-            int c= a[1];
-            if (r != 0 && board[r - 1][c] == 'O') {
-                queue.offer(new int[] { r - 1, c });
-                board[r - 1][c] ='#';
-            }
-            if (r != m - 1 && board[r + 1][c] == 'O') {
-                queue.offer(new int[] { r + 1, c });
-                board[r + 1][c] = '#';
-            }
-
-            if (c != 0 && board[r][c - 1] == 'O') {
-                queue.offer(new int[] { r, c - 1 });
-                board[r][c - 1] = '#';
-            }
-
-            if (c != n - 1 && board[r][c + 1] == 'O') {
-                queue.offer(new int[] { r, c + 1 });
-                board[r][c + 1] = '#';
-            }
-        }
+        // Flip surrounded regions
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') 
+
+                if (board[i][j] == 'O')
                     board[i][j] = 'X';
-                else if(board[i][j]=='#')
-                    board[i][j]='O';
+
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
             }
         }
+    }
+
+    private void dfs(int r, int c, char[][] board) {
+
+        if (r < 0 || r >= board.length ||
+            c < 0 || c >= board[0].length ||
+            board[r][c] != 'O')
+            return;
+
+        board[r][c] = '#';
+
+        dfs(r - 1, c, board);
+        dfs(r + 1, c, board);
+        dfs(r, c - 1, board);
+        dfs(r, c + 1, board);
     }
 }
