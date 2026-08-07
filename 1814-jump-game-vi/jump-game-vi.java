274434dca28cf069b13e351885863025cf153095
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-08 00:30:42 +0530

    Time: 24 ms (69.59%) | Memory: 84.7 MB (58.84%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.java
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -3,10 +3,10 @@ class Solution {
         int n= nums.length;
         Deque<Integer> dq= new ArrayDeque<>();
         int dp[] = new int[n];
-        dq.offer(n-1);
-        dp[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            while(!dq.isEmpty() && dq.peekFirst()>i+k){
+        dq.offer(0);
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            while(!dq.isEmpty() && dq.peekFirst()<i-k){
                 dq.pollFirst();
             }
             dp[i]=nums[i]+dp[dq.peek()];
@@ -16,7 +16,7 @@ class Solution {
             }
             dq.offerLast(i);
         }
-        return dp[0];
+        return dp[n-1];
         
     }
 }
