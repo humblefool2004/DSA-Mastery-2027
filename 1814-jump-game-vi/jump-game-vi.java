8b79160020c87c3c53e5c4c90c4a5086f5c2dcb2
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-08 00:34:10 +0530

    Time: 23 ms (86.61%) | Memory: 88.7 MB (25.62%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.java
+++ 1814-jump-game-vi/jump-game-vi.java
@@ -2,21 +2,19 @@ class Solution {
     public int maxResult(int[] nums, int k) {
         int n= nums.length;
         Deque<Integer> dq= new ArrayDeque<>();
-        int dp[] = new int[n];
         dq.offer(0);
-        dp[0] = nums[0];
         for(int i=1;i<n;i++){
             while(!dq.isEmpty() && dq.peekFirst()<i-k){
                 dq.pollFirst();
             }
-            dp[i]=nums[i]+dp[dq.peek()];
+            nums[i]+=nums[dq.peek()];
             
-            while(!dq.isEmpty() && dp[dq.peekLast()] <= dp[i]){
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]){
                 dq.pollLast();
             }
             dq.offerLast(i);
         }
-        return dp[n-1];
+        return nums[n-1];
         
     }
 }
