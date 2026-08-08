b69bd597e3bb75bac4e99a1c6cea03d71470ac62
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-08 17:15:54 +0530

    Time: 17 ms (50.54%) | Memory: 50.8 MB (69.43%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,38 +1,39 @@
-// class Solution {
-//     public int kthSmallest(int[][] matrix, int k) {
-//         int n= matrix.length;
-//         PriorityQueue<int[]> pq=  new PriorityQueue<>((a,b)-> a[2]-b[2]);
-        
-//         for(int i=0;i<n;i++){
-//             pq.offer(new int[]{i,0,matrix[i][0]});
-//         }
-
-//         while(k-- > 1){
-//             int curr[]= pq.poll();
-//             int r=curr[0];
-//             int c= curr[1];
-//             if(c+1<n){
-//                 pq.offer(new int[]{r,c+1,matrix[r][c+1]});
-//             }
-//         }
-//         return pq.peek()[2];
-//     }
-// }
-
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq=  new PriorityQueue<>((a,b)-> b-a);
         int n= matrix.length;
+        PriorityQueue<int[]> pq=  new PriorityQueue<>((a,b)-> a[2]-b[2]);
+        
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(pq.size()<k) pq.offer(matrix[i][j]); 
-                else if(pq.size()==k && pq.peek() > matrix[i][j]){
-                    pq.poll();
-                    pq.offer(matrix[i][j]); 
-                }
-                               
+            pq.offer(new int[]{i,0,matrix[i][0]});
+        }
+
+        while(k-- > 1){
+            int curr[]= pq.poll();
+            int r=curr[0];
+            int c= curr[1];
+            if(c+1<n){
+                pq.offer(new int[]{r,c+1,matrix[r][c+1]});
             }
         }
-        return pq.peek();
+        return pq.peek()[2];
     }
 }
+
+//general solution.
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         PriorityQueue<Integer> pq=  new PriorityQueue<>((a,b)-> b-a);
+//         int n= matrix.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(pq.size()<k) pq.offer(matrix[i][j]); 
+//                 else if(pq.size()==k && pq.peek() > matrix[i][j]){
+//                     pq.poll();
+//                     pq.offer(matrix[i][j]); 
+//                 }
+                               
+//             }
+//         }
+//         return pq.peek();
+//     }
+// }
