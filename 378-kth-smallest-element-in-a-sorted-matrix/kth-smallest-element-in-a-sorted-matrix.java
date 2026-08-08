7ff9c94f4d91107af3fe7fb325109ec4db5a5a91
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-08 17:00:26 +0530

    Time: 22 ms (28.58%) | Memory: 50.6 MB (84.88%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,17 +1,26 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq=  new PriorityQueue<>((a,b)-> b-a);
         int n= matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(pq.size()<k) pq.offer(matrix[i][j]); 
-                else if(pq.size()==k && pq.peek() > matrix[i][j]){
-                    pq.poll();
-                    pq.offer(matrix[i][j]); 
-                }
-                               
+        PriorityQueue<int[]> pq=  new PriorityQueue<>((a,b)-> a[2]-b[2]);
+        boolean visited[][]= new boolean[n][n];
+        
+        pq.offer(new int[]{0,0,matrix[0][0]});
+        visited[0][0]=true;
+        for(int i=0;i<k-1;i++){
+            int arr[]= pq.poll();
+            int r= arr[0];
+            int c= arr[1];
+            
+            if(r+1 <n && !visited[r+1][c]){
+                pq.offer(new int[]{r+1,c,matrix[r+1][c]});
+                visited[r+1][c]=true;
             }
+            if(c+1<n && !visited[r][c+1]){
+                pq.offer(new int[]{r,c+1,matrix[r][c+1]});
+                visited[r][c+1]=true;
+            }
+            
         }
-        return pq.peek();
+        return pq.peek()[2];
     }
 }
