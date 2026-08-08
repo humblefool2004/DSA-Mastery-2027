b1a2b50166b3e39e4aa745c0950942e84c59f02a
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-08 17:41:54 +0530

    Time: 1 ms (67.46%) | Memory: 51.9 MB (28.60%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,23 +1,62 @@
+//binary search
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n= matrix.length;
-        PriorityQueue<int[]> pq=  new PriorityQueue<>((a,b)-> a[2]-b[2]);
         
-        for(int i=0;i<n;i++){
-            pq.offer(new int[]{i,0,matrix[i][0]});
-        }
-
-        while(k-- > 1){
-            int curr[]= pq.poll();
-            int r=curr[0];
-            int c= curr[1];
-            if(c+1<n){
-                pq.offer(new int[]{r,c+1,matrix[r][c+1]});
+        int n=matrix.length;
+        int low= matrix[0][0];
+        int high=matrix[n-1][n-1];
+        
+        while(low<high){
+            int mid=low+ (high-low)/2;
+            int count=0;
+            for(int i=0;i<n;i++){
+                count+= count(matrix[i],mid);
+            }
+            if(count<k){
+                low=mid+1;
+            }else{
+                high=mid;
             }
         }
-        return pq.peek()[2];
+        return low;
+    }
+
+    private int count(int[] row, int mid){
+        int low=0;
+        int high=row.length;
+        while(low<high){
+            int m= low+ (high-low)/2;
+            if(row[m] <=mid){
+                low=m+1;
+            }else{
+                high=m;
+            }
+        }
+        return low;
     }
 }
+
+//kway merge
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         int n= matrix.length;
+//         PriorityQueue<int[]> pq=  new PriorityQueue<>((a,b)-> a[2]-b[2]);
+        
+//         for(int i=0;i<n;i++){
+//             pq.offer(new int[]{i,0,matrix[i][0]});
+//         }
+
+//         while(k-- > 1){
+//             int curr[]= pq.poll();
+//             int r=curr[0];
+//             int c= curr[1];
+//             if(c+1<n){
+//                 pq.offer(new int[]{r,c+1,matrix[r][c+1]});
+//             }
+//         }
+//         return pq.peek()[2];
+//     }
+// }
 
 //general solution.
 // class Solution {
