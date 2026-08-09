ea270237b41bf63e582c34c060cc229828977ae0
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-09 14:01:27 +0530

    Time: 4 ms (94.08%) | Memory: 46.8 MB (66.94%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -8,6 +8,7 @@ class Solution {
         for(int i=0;i<prerequisites.length;i++){
             list.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
+
         Deque<Integer> st= new ArrayDeque<>();
 
         int[] state = new int[numCourses]; //0 not visited, 1 visiting, 2 visited.
@@ -26,15 +27,20 @@ class Solution {
     }
 
     private boolean dfs(int node,int[] state,List<List<Integer>> list, Deque<Integer> st){
-        if(state[node]==1) return false;
+        if(state[node]==1) return false; //cycle check
         if(state[node]==2) return true;
         
         state[node]=1;
+
         for(int num : list.get(node)){
+
             if(!dfs(num,state,list,st))
                 return false;
+
         }
+
         state[node]=2;
+
         st.push(node);
 
         return true;
