7aa2011466dd904d18867d98648c7bf7c0ea1c63
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-09 13:21:03 +0530

    Time: 4 ms (94.08%) | Memory: 47 MB (38.63%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,3 +1,4 @@
+//when node is visited, push it, if node is visiting and you find it again, there's a cycle, if not visited, set visiting and dfs its neighbour.
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> list= new ArrayList<>();
@@ -7,7 +8,7 @@ class Solution {
         for(int i=0;i<prerequisites.length;i++){
             list.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        Stack<Integer> st= new Stack<>();
+        Deque<Integer> st= new ArrayDeque<>();
 
         int[] state = new int[numCourses]; //0 not visited, 1 visiting, 2 visited.
 
@@ -24,7 +25,7 @@ class Solution {
         return ans;
     }
 
-    private boolean dfs(int node,int[] state,List<List<Integer>> list, Stack<Integer> st){
+    private boolean dfs(int node,int[] state,List<List<Integer>> list, Deque<Integer> st){
         if(state[node]==1) return false;
         if(state[node]==2) return true;
         
