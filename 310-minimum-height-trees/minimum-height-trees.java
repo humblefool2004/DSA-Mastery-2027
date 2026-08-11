77ba064c99baf026ba7cddb0daf169d9346ab84e
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-12 02:36:12 +0530

    Time: 17 ms (73.23%) | Memory: 63.2 MB (76.51%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -2,49 +2,35 @@
 //onion peel method, also note: mht has max 2 centroids. remove leafs again and again
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-
-        if(n == 1)
-            return List.of(0);
-
-        List<List<Integer>> list = new ArrayList<>();
-
-        for(int i = 0; i < n; i++)
+        if(n==1) return List.of(0);
+        List<List<Integer>> list= new ArrayList<>();
+        for(int i=0;i<n;i++){
             list.add(new ArrayList<>());
-
+        }
         for(int[] edge : edges){
             list.get(edge[0]).add(edge[1]);
             list.get(edge[1]).add(edge[0]);
         }
-
-        int[] degree = new int[n];
-        Deque<Integer> queue = new ArrayDeque<>();
-
-        for(int i = 0; i < n; i++){
-            degree[i] = list.get(i).size();
-
-            if(degree[i] == 1)
-                queue.offer(i);
+        int[] degree= new int[n];
+        Deque<Integer> queue= new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            degree[i]=list.get(i).size();
+            if(degree[i]==1) queue.add(i); 
         }
 
-        int remaining = n;
+        int remaining=n;
 
-        while(remaining > 2){
-
-            int size = queue.size();
-
-            for(int i = 0; i < size; i++){
-
-                int removed = queue.poll();
-
-                for(int num : list.get(removed)){
-                    if(--degree[num] == 1)
-                        queue.offer(num);
-                }
+        while(remaining>2){
+            int size= queue.size();
+            for(int i=0;i<size;i++){
+                int removed=queue.poll();
+                    for(int num : list.get(removed)){
+                        if(--degree[num]==1) queue.add(num);
+                    }
             }
-
-            remaining -= size;
+            remaining-=size;
         }
-
         return new ArrayList<>(queue);
+
     }
 }
