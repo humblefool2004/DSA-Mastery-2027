add355e99d558bec4c0a3121c174fde59c046c0e
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-12 23:15:11 +0530

    Time: 1494 ms (48.70%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 -- select customer_id, count(customer_id) as count_no_trans from Visits v left join Transactions t on v.visit_id=t.visit_id  where t.visit_id is null group by(v.customer_id);
 
-#Not IN is used between two queries i guess
+#Not IN is used inside where clause to check if the subquery or list is in or not;
 select customer_id, count(customer_id) as count_no_trans from Visits v where v.visit_id NOT IN (select visit_id from Transactions) group by customer_id;
