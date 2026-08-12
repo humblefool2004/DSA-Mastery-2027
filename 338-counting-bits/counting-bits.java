99d04d4f8910e70cb400a881e5f43f300a33f58c
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-13 00:16:32 +0530

    Time: 2 ms (95.70%) | Memory: 48.8 MB (31.96%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,23 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-        int ans[]= new int[n+1];
-        ans[0]=0;
-        for(int i=1;i<n+1;i++){
-            if(i%2==0){
-                ans[i]= ans[i/2];
-            }else{
-                ans[i]= ans[i/2]+1;
-            }
+        int dp[]= new int[n+1];
+        for(int i=1;i<=n;i++){
+            if(i%2==0) dp[i]=dp[i/2];
+            else dp[i]= dp[i/2]+1;
         }
-       // System.out.println(5>>>1); //unsigned  is positive 0 append krta hai aage
-        //System.out.println(-4>>1);  5^111  1 1 ->0
-      //  01 10 -> 1
-        //00 0
-        return ans;
+        return dp;
     }
 }
-
-
-
-
-
