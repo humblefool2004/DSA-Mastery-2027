60ba9a54102bb0e0d518175328506af426d7863a
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-12 13:20:13 +0530

    Time: 786 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 #DISTINCT KEYWORD GIVES DISTINCT VALUES
-SELECT DISTINCT author_id AS id FROM Views WHERE author_id=viewer_id ORDER BY author_id;
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id=viewer_id ORDER BY id;
 
 #use this when you want other functions like count and all
 -- select author_id as id from Views where author_id=viewer_id group by author_id ORDER BY author_id;
