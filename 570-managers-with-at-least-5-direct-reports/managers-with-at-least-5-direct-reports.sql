5abf2fae751791b297cfec08727ff6dfde7faf59
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-13 19:51:00 +0530

    Time: 499 ms (10.87%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-
+#Group by ke saath having clause use krna
 select name from
 (select managerId,count(managerId) as cnt from Employee group by managerId having cnt>=5) a
 INNER JOIN Employee e ON a.managerId=e.id;
