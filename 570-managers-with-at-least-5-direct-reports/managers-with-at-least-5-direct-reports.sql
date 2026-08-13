3757b5a8a20c3b94c2cdea20d260f8976765d6df
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-13 19:56:28 +0530

    Time: 486 ms (11.78%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-#Group by ke saath having clause use krna
-select name from
-(select managerId,count(managerId) as cnt from Employee group by managerId having cnt>=5) a
-INNER JOIN Employee e ON a.managerId=e.id;
+#Group by ke saath having clause use krna, having processes after groups are made
+SELECT name 
+FROM
+(SELECT managerId FROM Employee 
+    GROUP BY managerId 
+    HAVING COUNT(managerId)>=5) a
+INNER JOIN 
+Employee e ON a.managerId = e.id;
