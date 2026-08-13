1ff6afc116e5fd0eec75db520ba7e3c0f4e60cf3
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-13 23:31:24 +0530

    Time: 929 ms (10.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+SELECT ROUND(COUNT(a.player_id)/COUNT(b.player_id),2) AS fraction
+FROM Activity a 
+RIGHT JOIN 
+(SELECT player_id, 
+    MIN(event_date) AS first_login
+FROM Activity 
+GROUP BY player_id) b
+ON a.player_id= b.player_id AND DATEDIFF(event_date,first_login)=1;
