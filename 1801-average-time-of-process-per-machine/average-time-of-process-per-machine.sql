6dd28a74ef953fb65fa62e179cb511cb94642364
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-13 12:47:54 +0530

    Time: 284 ms (32.69%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,17 +1,30 @@
 # Write your MySQL query statement below
+#CASE WHEN CONDITION THEN STATEMENT END
+SELECT machine_id,
+       ROUND(
+         (SUM(CASE WHEN activity_type = 'end' THEN timestamp END) - 
+          SUM(CASE WHEN activity_type = 'start' THEN timestamp END)) 
+         / COUNT(DISTINCT process_id), 
+         3
+       ) AS processing_time
+FROM Activity
+GROUP BY machine_id;
 
-select
-s.machine_id, ROUND(AVG(e.timestamp-s.timestamp),3) as processing_time
-from
-Activity s
-inner join 
-Activity e
-on
-s.machine_id=e.machine_id
-AND
-s.process_id=e.process_id
-AND
-s.activity_type='start'
-AND
-e.activity_type='end'
-group by(s.machine_id);
+
+
+
+-- SELECT
+-- s.machine_id, ROUND(AVG(e.timestamp-s.timestamp),3) AS processing_time
+-- FROM
+-- Activity s
+-- INNER JOIN 
+-- Activity e
+-- ON
+-- s.machine_id=e.machine_id
+-- AND
+-- s.process_id=e.process_id
+-- WHERE
+-- s.activity_type='start'
+-- AND
+-- e.activity_type='end'
+-- group by(s.machine_id);
