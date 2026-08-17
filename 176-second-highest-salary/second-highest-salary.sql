30a4dc4a93e6ad3349fb02b06ef17f21ee931d9e
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-18 01:00:13 +0530

    Time: 270 ms (86.48%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT CASE WHEN COUNT(*)=0 THEN null ELSE salary END as SecondHighestSalary 
+SELECT MAX(salary) AS SecondHighestSalary 
 FROM
 (
 SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS row_num from Employee
 ) a
-WHERE row_num=2
-LIMIT 1;
+WHERE row_num=2;
