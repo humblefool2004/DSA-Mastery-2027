23bc57dd48438e315c4eb6ecc6002786fe68147b
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-18 00:49:48 +0530

    Time: 601 ms (93.36%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,6 +1,9 @@
 # Write your MySQL query statement below
+# sql cant delete from same subquery while processing it.
+#hence another subquery is required
 DELETE FROM Person WHERE id NOT IN 
 (
     SELECT id from
     (SELECT MIN(id) AS id FROM Person GROUP BY email) a
 );
+
