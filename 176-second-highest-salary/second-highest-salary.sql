d7f8798f82bf3d17435ef1779bc9abd3e4aeaf45
Saheel Mohanta <humblefool2004@gmail.com>
2026-08-18 01:06:56 +0530

    Time: 259 ms (94.97%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
 #MAX returns null if all values are null or there are no rows
-SELECT MAX(salary) AS SecondHighestSalary 
-FROM
-(
-SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS row_num from Employee
-) a
-WHERE row_num=2;
+-- SELECT MAX(salary) AS SecondHighestSalary 
+-- FROM
+-- (
+-- SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS row_num from Employee
+-- ) a
+-- WHERE row_num=2;
+
+SELECT (SELECT DISTINCT salary FROM Employee 
+ORDER BY salary DESC 
+LIMIT 1 OFFSET 1) AS SecondHighestSalary;
